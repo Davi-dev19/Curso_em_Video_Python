ac956128a3a121eac4c88fefd2787463886a7397
Davi-dev19 <davi.gomes.dev19@gmail.com>
2026-03-04 16:30:08 -0300

    Exercicio 078 Concluido

---
+++ mundopython3/aula_17_listas/desafios_17/desafio_078.py
@@ -0,0 +1,28 @@
+"""
+Desafio 078: Manipulação de Listas dinâmicas e mapeamento de múltiplos índices de valores extremos.
+"""
+
+numeros = []
+
+for indice in range(0, 5):
+    numeros.append(int(input("Digite um número: ")))
+print("-" * 30)
+
+print(f"Os número digitados que foram para lista:")
+for indice, numero in enumerate(numeros):
+    print(f"{numero}", end=', ' if indice < 4 else ' ')
+print("-" * 30)
+
+maior_valor = max(numeros)
+menor_valor = min(numeros)
+
+print(f"\nO maior valor da lista: {maior_valor}. Que se encontra nos indices: ", end="")
+for indice, valor in enumerate(numeros):
+    if maior_valor == valor:
+        print(f"{indice + 1}", end=" ")
+print("-" * 30)
+
+print(f"\nO menor valor da lista: {menor_valor}. Que se encontra nos indices: ",  end="")
+for indice, valor in enumerate(numeros):
+    if menor_valor == valor:
+        print(f"{indice + 1}", end=" ")
