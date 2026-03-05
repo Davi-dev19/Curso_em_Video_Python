84558e767d753808bdd7e352526af6cc201412af
Davi-dev19 <davi.gomes.dev19@gmail.com>
2026-03-05 00:16:04 -0300

    Exercicio 081 Concluido

---
+++ mundopython3/aula_17_listas/desafios_17/desafio_081.py
@@ -0,0 +1,23 @@
+"""
+Desafio 081: Análise quantitativa e qualitativa de dados em Listas dinâmicas.
+"""
+
+numeros = []
+
+while True:
+    continuar_parar = " "
+    numeros.append(int(input("Digite um numero: ")))
+
+    while continuar_parar not in "SN":
+        continuar_parar = str(input("Deseja continuar? [S/N] ")).strip().upper()[0]
+    if continuar_parar in "N":
+        break
+
+numeros.sort(reverse=True)
+print(f"A lista contem {len(numeros)} números.")
+print(f"A lista em ordem decrescente {numeros}")
+
+if 5 not in numeros:
+    print("A lista não contêm o número 5.")
+else:
+    print(f"A lista tem o número 5 e se encontra no indice {numeros.index(5) + 1}")
