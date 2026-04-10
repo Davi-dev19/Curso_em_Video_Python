f57902798be24e554625241b53671186f6190a23
Davi | davidev.tech <contatodavidev.tech@gmail.com>
2026-04-10 19:33:26 -0300

    Exercicio 104 Concluindo | Tratando dados de dentro da função

---
+++ mundopython3/aula_21_funcoes_2/desafios_21/desafio_104.py
@@ -0,0 +1,26 @@
+def tratar_dados():
+    """
+    --> Trata a entrada, obrigando que o return para o programa principal seja um (int)
+    :param: Nenhum Parametro.
+    :return numero: O valor inteiro para o programa principal"""
+    while True:
+        num = str(input("Digite um número inteiro: ")).strip()
+        if num.isnumeric():
+            numero = int(num)
+            break
+        else:
+            print("ERRO! Digite um número inteiro válido.")
+
+    return numero
+
+
+def exibir_dados(num):
+    """
+    --> Exibe o valor que foi informado.
+    :param num: Recebe n como parametro.
+    :return: Nenhum retorno."""
+    print(f"Você informou o número {num}")
+
+
+n = tratar_dados()
+exibir_dados(n)
