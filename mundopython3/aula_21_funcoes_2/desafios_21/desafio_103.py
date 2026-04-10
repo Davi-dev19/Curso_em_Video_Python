1931b41edfa4d34500dc5818016677aad8d953d9
Davi | davidev.tech <contatodavidev.tech@gmail.com>
2026-04-10 15:57:04 -0300

    Pequenos ajustes nas strings do exercicio 103

--- mundopython3/aula_21_funcoes_2/desafios_21/desafio_103.py
+++ mundopython3/aula_21_funcoes_2/desafios_21/desafio_103.py
@@ -1,14 +1,15 @@
-def analisar_ficha(jogador='desconhecido', gols = 0):
+def analisar_ficha(jogador='<desconhecido>', gols = 0):
     """
     --> Exibe os dados tratados.
     :param jogador: Recebe o nome do Jogador.
     :param gols: Recebe a quantidade de gols do Jogador.
     :return: Nenhum Retorno """
-    print(f"O Jogador {jogador} fez {gols} gols.")
+    print(f"O jogador {jogador} fez {gols} gol(s) no campeonato.")
 
 
-nome_jogador = str(input("Qual o nome do jogador? ")).strip()
-gols_digitados = str(input("Quantos gols o jogador fez? ")).strip()
+print("--" * 16)
+nome_jogador = str(input("Nome do jogador: ")).strip()
+gols_digitados = str(input("Números de gols: ")).strip()
 if gols_digitados.isnumeric():
     quantidade_gols = int(gols_digitados)
 else:
