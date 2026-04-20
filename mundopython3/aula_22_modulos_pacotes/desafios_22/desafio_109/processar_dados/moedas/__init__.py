728688d4001140b69b66a57bc024d17bf4d259c1
Davi | davidev.tech <contatodavidev.tech@gmail.com>
2026-04-20 01:21:49 -0300

    Exercicio 109 Concluido | Chamando uma função dentro de outra

---
+++ mundopython3/aula_22_modulos_pacotes/desafios_22/desafio_109/processar_dados/moedas/__init__.py
@@ -0,0 +1,55 @@
+def calcular_metade_valor(n, formatar=False):
+    """
+    --> Calcula a Matade do valor.
+    :param n: Recebe preco.
+    :return resposta.
+    """
+    resposta = n / 2
+    return resposta if formatar is False else formatar_preco(resposta)
+
+
+def calcular_dobro_valor(n, formatar=False):
+    """
+    --> Calcula o dobro do valor.
+    :param n: Recebe preco.
+    :return resposta.
+    """
+    resposta = n * 2
+    return resposta if formatar is False else formatar_preco(resposta)
+
+
+def aumentar_porcentagem_valor(n=0, taxa=10, formatar=False):
+    """
+    --> Calcula o valor + taxa sobre o valor.
+    :param n: Recebe preco.
+    :param taxa: taxa_aumento.
+    :return resposta."""
+    valor_aumentado = taxa/100 * n
+    resposta = n + valor_aumentado
+
+    return resposta if formatar is False else formatar_preco(resposta)
+
+
+def reduzir_porcentagem_valor(n=0, taxa=10, formatar=False):
+    """
+    --> Calcula o valor - taxa sobre o valor.
+    :param n: Recebe preco.
+    :param taxa: taxa_reduzir.
+    :return resposta.
+    """
+    valor_redutor = taxa/100 * n
+    resposta = n - valor_redutor
+
+    return resposta if formatar is False else formatar_preco(resposta)
+
+
+def formatar_preco(preco=0, simbolo_monetario="R$"):
+    """
+    Transforma um valor numérico em uma string formatada como moeda.
+    :param preco: O valor numérico a ser formatado.
+    :param simbolo_monetario: O símbolo da moeda (padrão R$).
+    :return: String formatada com o símbolo e vírgula.
+    """
+    valor_ajustado = f'{preco:.2f}'.replace(".", ",")
+    
+    return f'{simbolo_monetario}{valor_ajustado}'
